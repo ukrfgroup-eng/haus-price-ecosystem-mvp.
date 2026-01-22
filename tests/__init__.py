96b439398526488124a1e981c4e20e280d9492dd
ukrfgroup-eng <uk.rfgroup@gmail.com>
2026-01-23 00:11:08 +0300

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+python
+"""
+Инициализация тестового пакета
+"""
+
+import os
+import sys
+
+# Добавляем путь к проекту
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
