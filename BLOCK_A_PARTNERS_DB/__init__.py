cebd6824256fa65efa202ce6e9ab808fb9fb0d41
ukrfgroup-eng <uk.rfgroup@gmail.com>
2026-01-30 23:18:40 +0300

    Update __init__.py

--- BLOCK_A_PARTNERS_DB/__init__.py
+++ BLOCK_A_PARTNERS_DB/__init__.py
@@ -1,1 +1,19 @@
+"""
+Блок A: База партнеров + Верификация
 
+Основной модуль для управления партнерами, верификации и поиска.
+"""
+
+__version__ = "1.0.0"
+__author__ = "HausPrice Ecosystem Team"
+
+
+from .config import config, load_config, get_database_url
+
+
+# Экспортируем основные классы для удобного импорта
+__all__ = [
+    'config',
+    'load_config',
+    'get_database_url',
+]
